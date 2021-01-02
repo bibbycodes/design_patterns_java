37785848f3041b83cbc7016e023231838e33f652
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2021-01-02 15:43:01 +0000

    clarify singleton pattern comment

--- src/com/pl/DbSingleton.java
+++ src/com/pl/DbSingleton.java
@@ -1,9 +1,9 @@
-// The singleton design pattern is used to create a single instance that may have some state that is shared accross
+// The singleton design pattern is used to create a single instance that may have some state that is shared across
 // multiple resources in an application.
 // There will only ever be a single instance of the singleton (hence the name)
-// Examples may include a runtime, a logger
-// Drawbacks include that the application may be harder to test due to this pattern and it introduces a sort of
-// global state that may become hard to manage
+// Examples may include a runtime and a logger
+// Drawbacks include that the application may be harder to test due to different sections of the application relying on
+// the singleton, it also introduces a sort of global state that may become hard to manage.
 
 package com.pl;
 
